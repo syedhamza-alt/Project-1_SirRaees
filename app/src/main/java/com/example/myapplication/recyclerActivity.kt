7268510ee9c9afb5e53a09529a2080c7395d9091
syedhamza-alt <syedhamzaimran3@gmail.com>
2023-10-14 21:46:22 +0500

    Commit Assignment-02 , week-02

--- app/src/main/java/com/example/myapplication/recyclerActivity.kt
+++ app/src/main/java/com/example/myapplication/recyclerActivity.kt
@@ -15,24 +15,19 @@ class recyclerActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
-        // this creates a vertical layout Manager
        binding.recyclerview.layoutManager = LinearLayoutManager(this)
 
-        // ArrayList of class ItemsViewModel
         val data = ArrayList<ItemsViewModel>(
 
         )
 
-        // This loop will create 20 Views containing
-        // the image with the count of view
+
         for (i in 1..20) {
             data.add(ItemsViewModel(R.drawable.img_2, "Person " + i))
         }
 
-        // This will pass the ArrayList to our Adapter
         val adapter = CustomAdapter(data)
 
-        // Setting the Adapter with the recyclerview
         binding.recyclerview.adapter = adapter
 
     }
